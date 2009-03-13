0b819fa2b8190d6fb2e0c9b50336f4c9a5a205bb
Eric Durand <eric.durand@berkeley.edu>
2009-03-13 00:00:00 +0000

    version 1.4-3

--- R/maxlik.betasplit.R
+++ R/maxlik.betasplit.R
@@ -1,97 +1,98 @@
-maxlik.betasplit<-function(phylo,up=10,remove.outgroup=FALSE,confidence.interval="none",prob.conf.inter=.95,size.bootstrap=100)
+maxlik.betasplit<-function (phylo, up = 10, remove.outgroup = FALSE, confidence.interval = "none",
+    conf.level = 0.95, size.bootstrap = 100)
 {
-	
-	vrais.aldous.fin<-function(i,n,b)
-	{
-		return(beta(b+i+1,b+n-i+1)/beta(i+1,n-i+1))
-	}
-
-	bbalance<-function(phylo)
-	{
-		return (t(apply(balance(phylo),FUN=function(x){c(x[1],x[1]+x[2])},MARGIN=1)))
-	}
-
-	renorm.aldous<-function(n,beta)
-	{
-		return(sum(sapply(1:(n-1),FUN=vrais.aldous.fin,n=n,b=beta)))
-	}
-
-	vrais.aldous.renorm<-function(i,n,beta)
-	{
-		return(vrais.aldous.fin(i,n,beta)/renorm.aldous(n,beta))
-	}
-
-	logvrais.aldous.phylo<-function(b,phylo,remove.outgroup=TRUE,prbootstrap=FALSE)
-	{
-		if(prbootstrap)
-		{
-			bal<-bbalance(as.phylo(phylo))
-		}
-		else
-		{
-			if(class(phylo)=="treeshape")
-				bal<-bbalance(as.phylo(phylo))
-	
-			if(class(phylo)=="phylo")
-				bal<-bbalance(phylo)
-	
-			if(remove.outgroup)
-			{
-			if((bal[1,1]<=2)  || ((bal[1,2]-bal[1,1])<=2)) bal<-bal[-1,]
-			}
-		}
-		return (sum(log(apply(bal,FUN=function(x,b){return (vrais.aldous.renorm(x[1],x[2],b))},b=b,MARGIN=1))))
-	}
-
-	if(class(phylo)=="treeshape")
-		bal<-bbalance(as.phylo(phylo))
-	if(class(phylo)=="phylo")
-		bal<-bbalance(phylo)
-	if(class(phylo)!="phylo" && class(phylo)!="treeshape")
-	{
-		print("The phylogeny shall be of class phylo or treeshape")
-		return
-	}
-	
-	if(remove.outgroup)
-	{
-		if((bal[1,1]<=2)  || ((bal[1,2]-bal[1,1])<=2)) bal<-bal[-1,]
-	}
-
-	nb.tip<-max(bal[1,])
-	
-	optim_lik_aldous<-function(phylo,remove.outgroup,prbootstrap)
-	{
-		optimize(f=function(x){logvrais.aldous.phylo(x,phylo,remove.outgroup,prbootstrap)},lower=-2,upper=up,maximum=TRUE)
-		#optim(par=0,fn=function(x){logvrais.aldous.phylo(x,phylo,remove.outgroup,prbootstrap)}, 
-#method="L-BFGS-B",lower=-1.99,upper=up, hessian = TRUE,control=list(fnscale=-1))
-	}
-	res<-optim_lik_aldous(phylo,remove.outgroup,prbootstrap=FALSE)
-
-	if (confidence.interval=="bootstrap_param")
-	{
-		function_aux<-function(n,i)
-		{
-			if (i == 0 | i == n)
-            			return (0)
-			else
-				return(vrais.aldous.fin(i,n,res$maximum))
-				#return(vrais.aldous.fin(i,n,res$par))
-		}
-		print(res$maximum)
-		#print(res$par)
-		tree.boot<-rtreeshape(n=size.bootstrap,tip.number=nb.tip,FUN=function_aux)
-		thebeta<-sapply(tree.boot,FUN=function(x){optim_lik_aldous(x,remove.outgroup,prbootstrap=TRUE)$maximum})
-		#thebeta<-sapply(tree.boot,FUN=function(x){optim_lik_aldous(x,remove.outgroup,prbootstrap=T)$par})
-		up.conf<-1-((1-prob.conf.inter)/2)
-		low.conf<-(1-prob.conf.inter)/2
-		conf_interval<-quantile(thebeta,c(low.conf,up.conf))
-	}
-	if (confidence.interval=="none")
-	{
-		conf_interval<-NULL
-	}
-	return (list(max_lik=res$maximum,conf_interval=conf_interval))
-	#return (list(max_lik=res$par,conf_interval=conf_interval))
+    vrais.aldous.fin <- function(i, n, b) {
+       aux<-beta(b + i + 1, b + n - i + 1)/beta(i + 1, n -
+            i + 1)
+	if(is.na(aux) | (aux==Inf))
+		aux<-(i/n)^b*(1-i/n)^(b)
+	return(aux)
+    }
+    bbalance <- function(phylo) {
+        return(t(apply(balance(phylo), FUN = function(x) {
+            c(x[1], x[1] + x[2])
+        }, MARGIN = 1)))
+    }
+    renorm.aldous <- function(n, beta) {
+        return(sum(sapply(1:(n - 1), FUN = vrais.aldous.fin,
+            n = n, b = beta)))
+    }
+    vrais.aldous.renorm <- function(i, n, beta) {
+        return(vrais.aldous.fin(i, n, beta)/renorm.aldous(n,
+            beta))
+    }
+    logvrais.aldous.phylo <- function(b, phylo, remove.outgroup = TRUE) {
+            if (class(phylo) == "treeshape")
+                bal <- bbalance(as.phylo(phylo))
+            if (class(phylo) == "phylo")
+                bal <- bbalance(phylo)
+            if (remove.outgroup) {
+                if ((bal[1, 1] <= 2) || ((bal[1, 2] - bal[1,
+                  1]) <= 2))
+                  bal <- bal[-1, ]
+            }
+        return(sum(log(apply(bal, FUN = function(x, b) {
+            return(vrais.aldous.renorm(x[1], x[2], b))
+        }, b = b, MARGIN = 1))))
+    }
+     logvrais.aldous.bal <- function(b,bal) {
+        return(sum(log(apply(bal, FUN = function(x, b) {
+            return(vrais.aldous.renorm(x[1], x[2], b))
+        }, b = b, MARGIN = 1))))
+    }
+    if (class(phylo) == "treeshape")
+        bal <- bbalance(as.phylo(phylo))
+    if (class(phylo) == "phylo")
+        bal <- bbalance(phylo)
+    if (class(phylo) != "phylo" && class(phylo) != "treeshape") {
+        print("The phylogeny shall be of class phylo or treeshape")
+        return
+    }
+    if (remove.outgroup) {
+        if ((bal[1, 1] <= 2) || ((bal[1, 2] - bal[1, 1]) <= 2))
+            bal <- bal[-1, ]
+    }
+    nb.tip <- max(bal[1, ])
+    optim_lik_aldous <- function(phylo, remove.outgroup) {
+        optimize(f = function(x) {
+            logvrais.aldous.phylo(x, phylo, remove.outgroup)
+        }, lower = -2, upper = up, maximum = TRUE)
+    }
+    optim_lik_aldous_bal <- function(bal) {
+        optimize(f = function(x) {
+            logvrais.aldous.bal(x,bal)
+        }, lower = -2, upper = up, maximum = TRUE)
+    }
+    res <- optim_lik_aldous(phylo, remove.outgroup)
+    if (confidence.interval == "bootstrap") {
+    	nb_b<-dim(bal)[1]
+    	fun_aux<-function()
+    	{
+    		optim_lik_aldous_bal(bal[sample(1:nb_b,size=nb_b,replace=TRUE),])$maximum
+    		
+    	}
+        thebeta <- replicate(size.bootstrap,fun_aux())
+        up.conf <- 1 - ((1 - conf.level)/2)
+        low.conf <- (1 - conf.level)/2
+        conf_interval <- quantile(thebeta, c(low.conf, up.conf))
+    }
+    if (confidence.interval == "profile") {
+    if( (res$objective-1.92)-logvrais.aldous.phylo(-2, phylo, remove.outgroup) <0)
+    	low.conf <-(-2)
+    else
+      low.conf <-uniroot(f = function(x) {
+            (res$objective-1.92)-logvrais.aldous.phylo(x, phylo, remove.outgroup)
+        }, lower = -2, upper = res$maximum)$root
+    if( (res$objective-1.92)-logvrais.aldous.phylo(up, phylo, remove.outgroup) <0)
+    	up.conf <-up
+    else
+      up.conf <-uniroot(f = function(x) {
+            (res$objective-1.92)-logvrais.aldous.phylo(x, phylo, remove.outgroup)
+        }, lower = res$maximum, upper = up)$root
+       conf_interval <-c(low.conf,up.conf)
+    }
+    if (confidence.interval == "none") {
+        conf_interval <- NULL
+    }
+    return(list(max_lik = res$maximum, conf_interval = conf_interval))
 }
-
